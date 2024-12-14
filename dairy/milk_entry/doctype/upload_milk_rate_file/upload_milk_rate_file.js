0f6fe00c135115ff0f4c17169672c25c50cd2a50
tabasum-khan <tabassumkhan020@gmail.com>
2024-12-15 00:04:19 +0530

    Initial commit after reinitializing Git

---
+++ dairy/milk_entry/doctype/upload_milk_rate_file/upload_milk_rate_file.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Dexciss Technology Pvt Ltd and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Upload Milk Rate File', {
+	// refresh: function(frm) {
+
+	// }
+});
