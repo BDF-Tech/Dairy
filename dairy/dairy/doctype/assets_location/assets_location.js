4db9c25f02ab305551db01677b9a4cf1bdb2345c
Sanskar199 <sanskar19shukla@gmail.com>
2026-02-10 18:45:33 +0530

    Create new Doctype Assets location

---
+++ dairy/dairy/doctype/assets_location/assets_location.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Dexciss Technology Pvt Ltd and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Assets Location", {
+// 	refresh(frm) {
+
+// 	},
+// });
