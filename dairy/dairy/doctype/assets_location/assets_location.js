ad11df3c5d6705c51156d55b601e545552645820
Sanskar199 <sanskar19shukla@gmail.com>
2026-02-11 16:34:26 +0530

    `Added setup function to Assets Location form to filter assets_code field and added new fields to the form grid`

--- dairy/dairy/doctype/assets_location/assets_location.js
+++ dairy/dairy/doctype/assets_location/assets_location.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2026, Dexciss Technology Pvt Ltd and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Assets Location", {
-// 	refresh(frm) {
-
-// 	},
-// });
+ frappe.ui.form.on("Assets Location", {
+setup: function(frm) {
+        frm.set_query("assets_code", function() {
+            return {
+                filters: {
+                    item_group: "Asset"
+                }
+            };
+        });
+    }
+ });
